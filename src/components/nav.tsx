6ee0e843fd258cfe332bd30d9f1303b68a814aa7
Daniel Bergholz <bergholz.daniel@gmail.com>
2024-09-06 15:29:25 -0300

    Add nav component

---
+++ src/components/nav.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+
+export function Nav() {
+  const pathname = usePathname()
+
+  const activeStyle = (path: string) =>
+    pathname === path ? "" : "opacity-60 hover:opacity-100 transition-opacity"
+
+  return (
+    <nav className="flex justify-between items-center md:text-xl">
+      <Link href="/" className="font-bold">
+        BERGHOLZ
+      </Link>
+      <ul className="flex space-x-4">
+        <li>
+          <Link href="/links" className={activeStyle("/links")}>
+            links
+          </Link>
+        </li>
+        <li>
+          <Link href="/blog" className={activeStyle("/blog")}>
+            blog
+          </Link>
+        </li>
+        <li>
+          <Link href="/courses" className={activeStyle("/courses")}>
+            courses
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
