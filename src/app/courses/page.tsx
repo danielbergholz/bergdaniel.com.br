7c7313506c51974b8ea8b6be86a5339f5a52ec92
Daniel Bergholz <bergholz.daniel@gmail.com>
2024-09-06 15:31:44 -0300

    Add initial courses page

---
+++ src/app/courses/page.tsx
@@ -0,0 +1,14 @@
+import type { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "Courses | Daniel Bergholz",
+  description: "Programming courses"
+}
+
+export default function Courses() {
+  return (
+    <main>
+      <h1>Courses page</h1>
+    </main>
+  )
+}
