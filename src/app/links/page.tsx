1ba904d1306fa88c651ecb29260efbc00d949876
Daniel Bergholz <bergholz.daniel@gmail.com>
2024-09-06 16:27:05 -0300

    Add links page

--- src/app/links/page.tsx
+++ src/app/links/page.tsx
@@ -1,4 +1,13 @@
 import type { Metadata } from "next"
+import {
+  DevTo,
+  GitHub,
+  LinkedIn,
+  Thunder,
+  Twitter,
+  YouTube
+} from "@/components/icons"
+import { Link } from "@/components/link"
 
 export const metadata: Metadata = {
   title: "Links | Daniel Bergholz",
@@ -7,8 +16,38 @@ export const metadata: Metadata = {
 
 export default function Links() {
   return (
-    <main>
-      <h1>Links page</h1>
+    <main className="my-32 md:my-44">
+      <section
+        aria-label="Social Media Links"
+        className="flex flex-col items-center gap-4"
+      >
+        <Link title="TechSchool" href="https://techschool.dev/">
+          <Thunder width={25} height={25} />
+        </Link>
+
+        <Link href="https://www.youtube.com/@DanielBergholz" title="YouTube">
+          <YouTube width={25} height={25} />
+        </Link>
+
+        <Link href="https://github.com/danielbergholz" title="GitHub">
+          <GitHub width={25} height={25} />
+        </Link>
+
+        <Link href="https://dev.to/danielbergholz" title="Blog">
+          <DevTo width={20} height={20} />
+        </Link>
+
+        <Link href="https://twitter.com/danielberg_" title="Twitter">
+          <Twitter width={20} height={20} />
+        </Link>
+
+        <Link
+          href="https://www.linkedin.com/in/daniel-gobbi-bergholz/"
+          title="LinkedIn"
+        >
+          <LinkedIn width={25} height={25} />
+        </Link>
+      </section>
     </main>
   )
 }
