e2fcf7f2682f6fb135752c0c2cad3e46d936b28b
Daniel Bergholz <bergholz.daniel@gmail.com>
2024-09-06 15:31:54 -0300

    Add initial links page

---
+++ src/app/links/page.tsx
@@ -0,0 +1,14 @@
+import type { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "Links | Daniel Bergholz",
+  description: "Social media links: YouTube, Twitter, Blog and GitHub"
+}
+
+export default function Links() {
+  return (
+    <main>
+      <h1>Links page</h1>
+    </main>
+  )
+}
