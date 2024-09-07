c9bbf0da4dbf8e998a443a2f7de51a12a89a4962
Daniel Bergholz <bergholz.daniel@gmail.com>
2024-09-07 15:52:59 -0300

    Add Article type

--- src/data-access/blog.ts
+++ src/data-access/blog.ts
@@ -1,17 +1,8 @@
+import { Article } from "@/lib/types"
+
 const USERNAME = "danielbergholz"
 const BASE_URL = "https://dev.to/api"
 const ARTICLES_URL = `${BASE_URL}/articles?username=${USERNAME}`
-
-type Article = {
-  id: number
-  title: string
-  description: string
-  readable_publish_date: string
-  url: string
-  social_image: string
-  reading_time_minutes: number
-  tag_list: string[]
-}
 
 export const getArticles = async () => {
   const response = await fetch(ARTICLES_URL)
---
+++ src/lib/types.ts
@@ -0,0 +1,10 @@
+export type Article = {
+  id: number
+  title: string
+  description: string
+  readable_publish_date: string
+  url: string
+  social_image: string
+  reading_time_minutes: number
+  tag_list: string[]
+}
