dca974af314c2492dabbe974b5edd48881b8c37d
Daniel Bergholz <bergholz.daniel@gmail.com>
2024-09-30 16:16:21 -0300

    Create HamburguerMenu component

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -14,6 +14,7 @@ export function Nav() {
       <Link href="/" className="font-bold">
         BERGHOLZ
       </Link>
+
       <ul className="flex space-x-2 md:space-x-4">
         <li>
           <Link href="/links" className={activeStyle("/links")}>
