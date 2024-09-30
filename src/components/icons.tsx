711ca28de2d91af0d7050c0f333159acbdf700fd
Daniel Bergholz <bergholz.daniel@gmail.com>
2024-09-30 16:15:39 -0300

    Add Hamburguer and X icons

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -172,3 +172,45 @@ export function YouTube({ fill, ...rest }: React.SVGProps<SVGSVGElement>) {
     </svg>
   )
 }
+
+export function Hamburguer(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="size-8"
+      aria-label="Hamburguer"
+      {...props}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+      />
+    </svg>
+  )
+}
+
+export function X(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="size-8"
+      aria-label="Close"
+      {...props}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M6 18 18 6M6 6l12 12"
+      />
+    </svg>
+  )
+}
