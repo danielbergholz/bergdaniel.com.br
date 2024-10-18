c7b4ce6e05fb0bb34ad6f9b2fbf32303fd1bcfaa
Daniel Bergholz <bergholz.daniel@gmail.com>
2024-10-18 16:55:24 -0300

    Add OG image to courses

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -5,7 +5,31 @@ import { Video } from "@/components/video"
 
 export const metadata: Metadata = {
   title: "Courses | Daniel Bergholz",
-  description: "Daniel Bergholz's courses"
+  description: "Free and premium fullstack web development courses",
+  openGraph: {
+    type: "website",
+    siteName: "Daniel Bergholz",
+    title: "Courses | Daniel Bergholz",
+    url: "https://bergdaniel.com.br/courses",
+    description: "Free and premium fullstack web development courses",
+    images: {
+      url: "https://bergdaniel.com.br/og_courses.png",
+      width: 1200,
+      height: 630
+    }
+  },
+  twitter: {
+    site: "@danielbergholz",
+    creator: "@danielbergholz",
+    card: "summary_large_image",
+    title: "Courses | Daniel Bergholz",
+    description: "Free and premium fullstack web development courses",
+    images: {
+      url: "https://bergdaniel.com.br/og_courses.png",
+      width: 1200,
+      height: 630
+    }
+  }
 }
 
 export const revalidate = 60 * 60 * 24 // 1 day
